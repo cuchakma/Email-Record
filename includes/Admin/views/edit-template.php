9d955a7c7f106da9dd65544f11c12c671e910f37
Cupid Chakma <greendaycu20@gmail.com>
2021-05-05 19:50:10 +0600

    added form

--- includes/Admin/views/edit-template.php
+++ includes/Admin/views/edit-template.php
@@ -1,1 +1,45 @@
-<h1>Hello</h1>
+	<h3>Edit Email</h3>
+	<form action="" method="post">
+		<table class="form-table" role="email-templates">
+			<tbody>
+				<tr>
+					<th scope="row">
+						<label for="to_email"><?php echo 'To Email' ?></label>
+					</th>
+					<td>
+						<input type="text" name="to_email" class="regular-text" value="">
+					</td>
+				</tr>
+				<tr>
+					<th scope="row">
+						<label for="subject"><?php echo 'Subject' ?></label>
+					</th>
+					<td>
+						<input type="text" name="subject" class="regular-text" value="">
+					</td>
+				</tr>
+				<tr>
+					<th scope="row">
+						<label for="ip_address"><?php echo 'Ip Address' ?></label>
+					</th>
+					<td>
+						<input type="text" name="ip_address" class="regular-text" value="">
+					</td>
+				</tr>
+				<tr>
+					<th scope="row">
+						<label for="sent_date"><?php echo 'Sent Date' ?></label>
+					</th>
+					<td>
+						<input type="date" name="sent_date" class="regular-text" value="">
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</form>
+<?php
+	$settings = array(
+		'media_buttons',
+		'editor_css'
+	);
+	wp_editor( '', 'description', $settings );
